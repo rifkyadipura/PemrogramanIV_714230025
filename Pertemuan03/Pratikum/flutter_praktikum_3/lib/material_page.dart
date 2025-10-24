63657f4094503fb016919120b90380f7c95dac84
rifkyadipura <rifkyadipura@gmail.com>
2025-10-24 13:30:43 +0700

    Upload Praktikum 03

---
+++ Pertemuan03/Pratikum/flutter_praktikum_3/lib/material_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(title: const Text('Item 1')),
+            ListTile(title: const Text('Item 2')),
+          ],
+        ),
+      ),
+      body: const Center(child: BiggerText(text: 'Hello ULBI')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: Text(_textSize == 16.0 ? "Perbesar" : "Perkecil"),
+          onPressed: () {
+            setState(() {
+              _textSize = _textSize == 16.0 ? 32.0 : 16.0;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
